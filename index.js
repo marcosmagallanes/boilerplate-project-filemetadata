186270225202b57ba1bbfa6bb617679d581b842e
Marcos <marcos.m.magallanes@gmail.com>
2022-10-01 10:24:20 -0600

    working state

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,6 +13,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+
+  const { originalname, mimetype, size } = req.file
+
+  try {
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size,
+    });
+  } catch (err) {
+    console.error(err)
+    res.json({ error: err})    
+  }
+})
 
 
 
